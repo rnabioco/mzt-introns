2caaf02194a559bc0695620b11eee7f1084a188e
Kent Riemondy <kent.riemondy@gmail.com>
2021-09-14 09:05:00 -0600

    reorg cluster related settings

--- pipeline/snakecharmer.sh
+++ pipeline/snakecharmer.sh
@@ -12,14 +12,14 @@ set -o nounset -o pipefail -o errexit -x
 args=' -q rna 
        -o {log}.out 
        -e {log}.err 
-       -J {params.job_name} 
-       -R "{params.memory} span[hosts=1] " 
+       -J {rule} 
+       -R "select[mem>{resources.mem_mb}] rusage[mem={resources.mem_mb}] span[hosts=1] " 
        -n {threads}  ' 
     
 #### load necessary programs ####
 
-# If programs are not all in the path then modify code to load 
-# the necessary programs
+# If programs are not all in the path then load 
+# the necessary programs or place in PATH
 
 # load modules
 . /usr/share/Modules/init/bash
@@ -41,4 +41,4 @@ snakemake --drmaa "$args" \
     --latency-wait 50 \
     --printshellcmds \
     --rerun-incomplete  \
-    --configfile config-drosophila.yaml
+    --configfile config-test.yaml 
