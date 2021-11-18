e16e8d7f56a1b13e6db13fb696803368e605361d
Kent Riemondy <kent.riemondy@gmail.com>
2021-11-17 20:41:29 -0700

    add download script for small databases

--- dbases-test/drosophila/ext/dl_dbases.sh
+++ dbases-test/drosophila/ext/dl_dbases.sh
@@ -5,9 +5,9 @@ samtools faidx Drosophila_melanogaster.BDGP6.dna.toplevel.fa
 wget ftp://ftp.ensembl.org/pub/release-84/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.84.gtf.gz
 gunzip Drosophila_melanogaster.BDGP6.84.gtf.gz
 
-samtools faidx Drosophila_melanogaster.BDGP6.dna.toplevel.fa 4 > Drosophila_melanogaster.BDGP6.dna.chr4.fa
-samtools faidx Drosophila_melanogaster.BDGP6.dna.chr4.fa
+samtools faidx Drosophila_melanogaster.BDGP6.dna.toplevel.fa 2L > Drosophila_melanogaster.BDGP6.dna.2L.fa
+samtools faidx Drosophila_melanogaster.BDGP6.dna.2L.fa
 
-awk '$1 == "4"' Drosophila_melanogaster.BDGP6.84.gtf > Drosophila_melanogaster.BDGP6.84.chr4.gtf 
+awk '$1 == "2L"' Drosophila_melanogaster.BDGP6.84.gtf > Drosophila_melanogaster.BDGP6.84.2L.gtf 
 
 rm -f Drosophila_melanogaster.BDGP6.84.gtf Drosophila_melanogaster.BDGP6.dna.toplevel.fa Drosophila_melanogaster.BDGP6.dna.toplevel.fa.fai
