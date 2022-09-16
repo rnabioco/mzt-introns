948805a926ff677b20c815a32c5b213e3653d8a1
Kent Riemondy <kent.riemondy@gmail.com>
2022-09-16 14:39:06 -0600

    add more reporting on masking and fix upstream bug in fcount approach

--- R/mask_introns.R
+++ R/mask_introns.R
@@ -39,7 +39,8 @@ dir.create(file.path(outpath, "intron_mask"), recursive = TRUE, showWarnings = F
 fa_fn <- file.path(eisa_dir, "eisa.fa")
 eisa_seqs <- import(fa_fn, format = "fasta")
 
-# get coverage
+# get coverage and find seqs to mask ----------------------------------------------
+
 bw_ivls <- lapply(bws, read_bigwig)
 bw_ivls <- lapply(bw_ivls, function(x) {
   seqlevels(x) <- seqlevels(eisa_seqs)
@@ -59,28 +60,57 @@ masked_intron_ivls <- masked_ivls[str_detect(seqnames(masked_ivls), "-I[0-9]*$")
 export(masked_intron_ivls,
        file.path(outpath, "intron_mask", "intron_mask_txcoords_eisa_bt2_align.bed"))
 
-
-# get all introns to examine amount of masking
 dup_tx <- read.table(file.path(eisa_dir, "duplicated_seqs.tsv"), header = TRUE)
-# gtf_fn <- file.path(eisa_dir, "eisaR.gtf")
-# gtf <- import(gtf_fn)
-# 
-# intron_gtf <- gtf[gtf$type == "exon" & str_detect(gtf$ID, "-I[0-9]*$")]
-# txdb <-  makeTxDbFromGRanges(gtf)
-# tx <- transcripts(txdb)
-# intron_tx <- tx[str_detect(tx$tx_name, "-I[0-9]*$")]
-# names(intron_tx) <- intron_tx$tx_name
+gtf_fn <- file.path(eisa_dir, "eisaR.gtf")
+txdb <- makeTxDbFromGFF(gtf_fn)
+tx <- transcripts(txdb, use.names = TRUE)
+exons <- exons(txdb, use.names = TRUE)
+exons <- exons[!str_detect(names(exons), "-I[0-9]*")]
+exons <- reduce(unname(exons))
 
-intron_tx_coord <- GRanges(seqnames = names(eisa_seqs), 
+masked_genomic_intron_ivls <- reduce(mapFromTranscripts(masked_intron_ivls, tx))
+all_genomic_intron_ivls <- reduce(unname(tx[str_detect(names(tx), "-I[0-9]*"), ]))
+
+export(masked_genomic_intron_ivls,
+       file.path(outpath, "intron_mask", "intron_mask_genomiccoords_eisa_bt2_align.bed"))
+
+export(tx[str_detect(names(tx), "-I[0-9]*"), ],
+       file.path(outpath, "intron_mask", "eisa_introns.bed"))
+
+
+# Report amount of intron masking -----------------------------------------
+
+# - Report based on amount of genomic intronic sequence
+# - Exclude exon flank (+/- 40bp) from intron seq
+# - Report masking due to intron regions that overlap  exons from quant (masked due to exon overlap)
+
+# remove exon seqs (and removes flanks)
+hits <- findOverlaps(all_genomic_intron_ivls, exons)
+toSubtract <- reduce(extractList(exons, as(hits, "List")), 
+                     ignore.strand = TRUE)
+ans <- psetdiff(all_genomic_intron_ivls, toSubtract)
+all_genomic_intron_ivls <- unlist(ans[width(ans) > 0L])
+
+ans <- findOverlapPairs(masked_genomic_intron_ivls, all_genomic_intron_ivls)
+mcols(ans)$overlap_width <- width(pintersect(ans, ignore.strand = TRUE))
+masked_nts <- sum(mcols(ans)$overlap_width)
+
+prop_masked <- signif(sum(mcols(ans)$overlap_width) / sum(width(all_genomic_intron_ivls)), 3)
+
+message("proportion of genomic intron nucleotides masked to Ns: ", prop_masked )
+
+# Filter and mask sequences ------------------------------------------------
+
+eisa_intron_seqs <- eisa_seqs[str_detect(names(eisa_seqs), "-I[0-9]*")]
+intron_tx_coord <- GRanges(seqnames = names(eisa_intron_seqs), 
                            ranges = IRanges(start = 1, 
-                                            end = seqlengths(eisa_seqs)),
+                                            end = seqlengths(eisa_intron_seqs)),
                            strand = "+")
-seqlevels(intron_tx_coord) <- seqlevels(eisa_seqs)
-seqinfo(intron_tx_coord) <- seqinfo(eisa_seqs) 
+seqlevels(intron_tx_coord) <- seqlevels(eisa_intron_seqs)
+seqinfo(intron_tx_coord) <- seqinfo(eisa_intron_seqs) 
 
 ans <- findOverlapPairs(masked_intron_ivls, intron_tx_coord)
 mcols(ans)$overlap_width <- width(pintersect(ans, ignore.strand = TRUE))
-
 masked_nts <- unlist(lapply(split(mcols(ans)$overlap_width, seqnames(second(ans))),
                             sum))
 
@@ -102,12 +132,13 @@ tx_info$genome_masked <- genome_N_nts[tx_info$tx]
 tx_info$prop_masked <- (tx_info$genome_masked + tx_info$masked_nts) / tx_info$tx_len
 tx_info$unmasked_nts <- tx_info$tx_len - tx_info$genome_masked - tx_info$masked_nts
 
-# set some cutoffs for introns to include for quant
+# set cutoffs for introns to include for quant
 to_drop <- rownames(tx_info)[tx_info$prop_masked >= 0.80 | tx_info$unmasked_nts < 25]
 
 message("removing ", 
         length(to_drop), " (", signif(length(to_drop) / nrow(tx_info), 3), ")",
         " introns with:\n >= 80% masked sequence or < 25 nt unmasked sequence")
+
 prop_masked <- signif(sum(tx_info$masked_nts) / sum(tx_info$tx_len), 3)
 
 message("proportion of all intron nucleotides masked to Ns: ", prop_masked )
@@ -118,9 +149,10 @@ prop_expr_masked <- signif(sum(intron_expr$masked_nts) / sum(intron_expr$tx_len)
 message("proportion of intron nucleotides masked to Ns\nfor introns with at least 1 read: ",
         prop_expr_masked )
 
-# mask introns 
+# mask intron sequence -----------------------------------------------------------------
+
 irl_to_mask <- as(masked_intron_ivls, "IntegerRangesList")
-irl_to_mask <- irl_to_mask[elementNROWS(irl_to_mask) > 1]
+irl_to_mask <- irl_to_mask[elementNROWS(irl_to_mask) > 0]
 seqs_to_mask <- eisa_seqs[names(irl_to_mask)]
 
 mask_seqs <- as(strrep("N", width(masked_intron_ivls)), "DNAStringSet")
@@ -129,6 +161,7 @@ mask_seqs <- mask_seqs[names(irl_to_mask)]
 
 stopifnot(identical(names(mask_seqs), names(irl_to_mask)))
 stopifnot(identical(names(mask_seqs), names(seqs_to_mask)))
+
 # names not used in replaceAt
 irl_to_mask <- unname(irl_to_mask)
 masked_seqs <- replaceAt(seqs_to_mask, irl_to_mask, mask_seqs)
